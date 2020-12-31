32ce8a6dc106100b760358bc60215cd39516d627
Om Gupta <55027854+omgupta1608@users.noreply.github.com>
2020-12-31 21:54:18 +0530

    script updated

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:apimote/apimote.github.io.git master
+git push -f git@github.com:apimote/apimote.github.io.git master
 
 cd -
